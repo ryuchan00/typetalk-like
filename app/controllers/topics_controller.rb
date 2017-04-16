6bd14d947f158e18bb11cff7e0b4505d9f5d1941
yamakawa00 <leokun0210@gmail.com>
2017-04-16 10:40:09 +0900

    update

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,8 @@ class TopicsController < ApplicationController
   end
 
   def show
-    # @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:id])
+
     require 'net/https'
     require 'uri'
     require 'json'
@@ -12,7 +13,7 @@ class TopicsController < ApplicationController
 
     client_id = ENV['CLIENT_ID']
     client_secret = ENV['CLIENT_SECRET']
-    topic_id = topic.topidId
+    topic_id = @topic.topicId.to_s
 
 # setup a http client
     http = Net::HTTP.new('typetalk.in', 443)
@@ -38,7 +39,6 @@ class TopicsController < ApplicationController
       if post['likes'].count != 0 then
         created_time = post['createdAt']
         created_time_to_time = Time.parse(created_time).in_time_zone
-        puts created_time_to_time
 
         post_data = {
             "name" => post['account']['fullName'],
@@ -47,8 +47,8 @@ class TopicsController < ApplicationController
             "imageUrl" => post['account']['imageUrl'],
             "created_at" => created_time_to_time.to_s
         }
-        puts post['account']['fullName']
-        puts post_data['like']
+        # puts post['account']['fullName']
+        # puts post_data['like']
         @posts.push(post_data)
       end
       @posts = @posts.sort { |a, b| b['like'] <=> a['like'] }
@@ -62,7 +62,6 @@ class TopicsController < ApplicationController
   def create
     @topic = current_user.topics.build(topic_params)
     @topic.topicId = @topic.topicId.split('/').last
-    # p @topic.topicId.split('/').last
     if @topic.save
       flash[:success] = 'トピックを登録しました。'
       redirect_to @topic
