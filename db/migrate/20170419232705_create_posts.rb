54e164f0a0195f40a66c159eba6022e825df5c21
yamakawa00 <leokun0210@gmail.com>
2017-04-20 21:17:26 +0900

    up

---
+++ db/migrate/20170419232705_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :post_id
+      t.references :topic, foreign_key: true
+
+      t.timestamps
+
+      t.index [:post_id], unique: true
+    end
+  end
+end
